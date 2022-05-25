ff34f1510d049ff63d24ddf4f0e7f415d4857332
Ivan Zamula <ivan.zamula@gmail.com>
2022-05-25 13:17:05 +0300

    + Google OAUth2 allauth-django

--- core/settings.py
+++ core/settings.py
@@ -73,8 +73,8 @@ AUTHENTICATION_BACKENDS = [
 #
 # LOGIN_URL = '/auth/login/google-oauth2/'
 #
-# LOGIN_REDIRECT_URL = '/'
-# LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 #
 # SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
